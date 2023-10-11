101cb114984d4cae50152aa9f64a52c782f5cbeb
omar bouassida <57063744+Omar-bit@users.noreply.github.com>
2023-10-11 01:57:25 +0100

    initializing the repo

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeNonAuth from './src/screens/HomeNonAuth';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [user, setUser] = React.useState(null);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {!user && (
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeNonAuth} />
+        </Stack.Navigator>
+      )}
+      {user && (
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={() => <Text>Auth</Text>} />
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/HomeNonAuth.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const HomeNonAuth = () => {
+  return (
+    <View className='flex-1 items-center justify-right bg-red-800'>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default HomeNonAuth;

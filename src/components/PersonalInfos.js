17dce5241710ddc27a35b483b47788d2254d67b7
omar bouassida <57063744+Omar-bit@users.noreply.github.com>
2023-10-22 13:20:51 +0100

    fixing NaN bug

--- src/components/PersonalInfos.js
+++ src/components/PersonalInfos.js
@@ -15,7 +15,9 @@ const PersonalInfos = ({ personalInfos, setPersonalInfos, setCurrentStep }) => {
   };
   function modifyPersonal(i, val) {
     const newPersonalInfos = personalInfos.map((el, index) =>
-      i != index ? el : { ...el, answer: el.type ? parseInt(val) : val }
+      i != index
+        ? el
+        : { ...el, answer: el.type == 'numeric' ? parseInt(val || 0) : val }
     );
     setPersonalInfos(newPersonalInfos);
   }
@@ -36,7 +38,11 @@ const PersonalInfos = ({ personalInfos, setPersonalInfos, setCurrentStep }) => {
               <Text className='text-lg'>{question[lang].q}</Text>
               <TextInput
                 className='px-1 py-2 bg-[#eeeeeeee] border rounded-xl '
-                value={String(question.answer)}
+                value={
+                  String(question.answer) != 'NaN'
+                    ? String(question.answer)
+                    : ''
+                }
                 onChangeText={(text) => modifyPersonal(index, text)}
                 keyboardType={question.type ? 'numeric' : 'default'}
               />
